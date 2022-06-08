a39175eede552b3ebf877ce1d2c0cad32d21059a
Matthew Spach <af@Matthews-MacBook-Pro-2.local>
2022-06-08 09:31:33 -0700

    websockets not triggering

--- app/mailers/comments_mailer.rb
+++ app/mailers/comments_mailer.rb
@@ -5,9 +5,9 @@ class CommentsMailer < ApplicationMailer
   #
   #   en.comments_mailer.submitted.subject
   #
-  def submitted
-    @greeting = "Hi"
+  def submitted(comment)
+    @comment = comment
 
-    mail to: "to@example.org"
+    mail to: "matthewspach@gmail.com", subject: "New comment"
   end
 end
